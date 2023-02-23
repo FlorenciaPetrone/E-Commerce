434b198901d5145dce945da9497c41b99122b6a7
Florencia Petrone <petrone@fluur.de>
2023-02-23 12:23:56 +0100

    Products UI implementation

--- client/src/pages/Products/index.jsx
+++ client/src/pages/Products/index.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import List from '../../components/List'
+import "./index.scss"
 
 const Products = () => {
+  const params = useParams().id
+  const categoryId = parseInt(params)
+  const [maxPrice, setMaxPrice] = useState(1000);
+  const [sort, setSort] = useState(null);
+
   return (
-    <div>Products</div>
+    <div className='products'>
+      <div className="left">
+        <div className="filterItem">
+          <h2>Product Categories</h2>
+          <div className="inputItem">
+            <input type="checkbox" id="1" value={1}/>
+            <label htmlFor="" >Shoes</label>
+          </div>
+          <div className="inputItem">
+            <input type="checkbox" id="2" value={2}/>
+            <label htmlFor="" >Skirts</label>
+          </div>
+          <div className="inputItem">
+            <input type="checkbox" id="3" value={3}/>
+            <label htmlFor="" >Coats</label>
+          </div>
+        </div>
+        <div className="filterItem">
+          <h2>Filter by Price</h2>
+          <div className="inputItem">
+            <span>0</span>
+            <input className='range' type="range" min={0} max={1000} onChange={(e) => setMaxPrice(e.target.value)} />
+            <span>{maxPrice}</span>
+          </div>
+        </div>
+        <div className="filterItem">
+          <h2>Sort by</h2>
+          <div className="inputItem">
+            <input type="radio" id="asc" value="asc" name="price" onChange={e => setSort('asc')} />
+            <label htmlFor="asc">Price (Lowest First)</label>
+          </div>
+          <div className="inputItem">
+            <input type="radio" id="desc" value="desc" name="price" onChange={e => setSort('desc')} />
+            <label htmlFor="desc">Price (Highest First)</label>
+          </div>
+        </div>
+      </div>
+      <div className="right">
+        <img className="categoryImage" src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+        <List categoryId={categoryId} maxPrice={maxPrice} sort={sort} />
+      </div>
+    </div>
   )
 }
 
